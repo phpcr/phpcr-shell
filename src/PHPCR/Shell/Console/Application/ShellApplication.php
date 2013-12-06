d989623625f6e983ae9e1654d86d4ce8a4f8174a
dantleech <dan.t.leech@gmail.com>
2013-12-06 23:27:16 +0100

    Added shell exit command

--- src/PHPCR/Shell/Console/Application/ShellApplication.php
+++ src/PHPCR/Shell/Console/Application/ShellApplication.php
@@ -8,38 +8,42 @@ use PHPCR\SessionInterface;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-use PHPCR\Util\Console\Helper\PhpcrConsoleDumperHelper;
-use PHPCR\Shell\Console\Helper\ResultFormatterHelper;
-use PHPCR\Util\Console\Helper\PhpcrHelper;
-use PHPCR\Util\Console\Helper\PhpcrCliHelper;
-use PHPCR\Util\Console\Command\NodeDumpCommand;
 use PHPCR\Shell\Console\Command\Query\SelectCommand;
+use PHPCR\Shell\Console\Command\Shell\ChangePathCommand;
+use PHPCR\Shell\Console\Command\Shell\PwdCommand;
+use PHPCR\Shell\Console\Helper\ResultFormatterHelper;
+use PHPCR\Shell\PhpcrSession;
+use PHPCR\SimpleCredentials;
+use PHPCR\Util\Console\Command\NodeDumpCommand;
 use PHPCR\Util\Console\Command\NodeMoveCommand;
 use PHPCR\Util\Console\Command\NodeRemoveCommand;
-use PHPCR\Util\Console\Command\NodesUpdateCommand;
 use PHPCR\Util\Console\Command\NodeTouchCommand;
 use PHPCR\Util\Console\Command\NodeTypeListCommand;
 use PHPCR\Util\Console\Command\NodeTypeRegisterCommand;
+use PHPCR\Util\Console\Command\NodesUpdateCommand;
 use PHPCR\Util\Console\Command\WorkspaceCreateCommand;
 use PHPCR\Util\Console\Command\WorkspaceDeleteCommand;
 use PHPCR\Util\Console\Command\WorkspaceExportCommand;
 use PHPCR\Util\Console\Command\WorkspaceImportCommand;
 use PHPCR\Util\Console\Command\WorkspaceListCommand;
 use PHPCR\Util\Console\Command\WorkspacePurgeCommand;
+use PHPCR\Util\Console\Helper\PhpcrCliHelper;
+use PHPCR\Util\Console\Helper\PhpcrConsoleDumperHelper;
+use PHPCR\Util\Console\Helper\PhpcrHelper;
 use Symfony\Component\Console\Command\Command;
-use PHPCR\Shell\Console\Command\Shell\ChangePathCommand;
-use PHPCR\Shell\Console\Command\Shell\PwdCommand;
 use Symfony\Component\Console\Input\ArrayInput;
+use PHPCR\Shell\Console\Command\Shell\ExitCommand;
 
 class ShellApplication extends Application
 {
-    public function __construct(SessionInterface $session)
+    public function __construct(InputInterface $input)
     {
         parent::__construct('PHPCR', '1.0');
 
         $this->add(new SelectCommand());
         $this->add(new ChangePathCommand());
         $this->add(new PwdCommand());
+        $this->add(new ExitCommand());
 
         $this->add($this->wrap(new NodeDumpCommand())
             ->setName('ls')
@@ -85,10 +89,50 @@ class ShellApplication extends Application
             ->setName('workspace-purge')
         );
 
+        $session = $this->getSession($input, $workspace);
+
         $this->getHelperSet()->set(new PhpcrConsoleDumperHelper());
         $this->getHelperSet()->set(new ResultFormatterHelper());
         $this->getHelperSet()->set(new PhpcrHelper($session));
         $this->getHelperSet()->set(new PhpcrCliHelper($session));
+    }
+
+    private function getSession($input)
+    {
+        $transport = $this->getTransport($input);
+        $repository = $transport->getRepository();
+        $credentials = new SimpleCredentials(
+            $input->getOption('phpcr-username'),
+            $input->getOption('phpcr-password')
+        );
+
+        $session = $repository->login($credentials, $input->getOption('phpcr-workspace'));
+        $session = new PhpcrSession($session);
+
+        return $session;
+    }
+
+    private function getTransport(InputInterface $input)
+    {
+        foreach (array(
+            new \PHPCR\Shell\Transport\DoctrineDbal($input),
+            new \PHPCR\Shell\Transport\Jackrabbit($input),
+        ) as $transport) {
+            $transports[$transport->getName()] = $transport;
+        }
+
+        $transportName = $input->getOption('transport');
+
+        if (!isset($transports[$transportName])) {
+            throw new \InvalidArgumentException(sprintf(
+                'Unknown transport "%s", I have "%s"',
+                $transportName, implode(', ', array_keys($transports))
+            ));
+        }
+
+        $transport = $transports[$transportName];
+
+        return $transport;
     }
 
     public function wrap(Command $command)
---
+++ src/PHPCR/Shell/Console/Command/Shell/ExitCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace PHPCR\Shell\Console\Command\Shell;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ExitCommand extends Command
+{
+    public function configure()
+    {
+        $this->setName('exit');
+        $this->setDescription('Logout and quit the shell');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->getHelper('phpcr')->getSession()->logout();
+        $output->writeln('<info>Bye!</info>');
+        exit(0);
+    }
+}
