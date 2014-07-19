eec078a46899e4ae71de2492b5bd6a8acc16a105
dantleech <dan.t.leech@gmail.com>
2014-07-19 09:44:13 +0200

    Updated version to alpha4

--- src/PHPCR/Shell/Console/Application/SessionApplication.php
+++ src/PHPCR/Shell/Console/Application/SessionApplication.php
@@ -16,7 +16,7 @@ use PHPCR\Shell\Console\Command\ShellCommand;
 class SessionApplication extends BaseApplication
 {
     const APP_NAME = 'PHPCRSH';
-    const APP_VERSION = '1.0.0-alpha3';
+    const APP_VERSION = '1.0.0-alpha4';
 
     protected $shellApplication;
 
